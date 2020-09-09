36ad28d9ce482a7f395085657b1d959945f73033
João Danilo Zucolotto Diedrich <jdzd@pop-os.localdomain>
2020-09-09 15:15:09 -0300

    agora vai

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
-app.use(express.static('assets'));
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 app.get('/', (req, res)=>{
     res.sendFile(__dirname + '/src/index.html');
 })
 
-const port = 80;
+const port = 8080;
 
 app.listen(port, () => {
     console.clear();
